f21d2278bbbfd8bfae9c87400d5bd7f93c6c2973
Hunter Suttle <huntermsuttle.tfa@gmail.com>
2023-06-10 14:20:27 -0600

    Added my own code for prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please insert your grade as a percentage: ");
+        string percent = Console.ReadLine();
+        double grade = double.Parse(percent);
+
+        string letter = "";
+
+        if (grade >= 90)
+            letter = "A";
+        else if (grade >= 80)
+            letter = "B";
+        else if (grade >= 70)
+            letter = "C";
+        else if (grade >= 60)
+            letter = "D";
+        else 
+            letter = "F";
+
+        if (letter == "A" || letter == "B" || letter == "C")
+            Console.Write("Congratulations! You passed the course! ");
+        else
+            Console.Write("Unfortunately you did not pass the class. Better luck next term! ");
+
+        Console.Write($"Your letter grade was {letter}.");
     }
 }
