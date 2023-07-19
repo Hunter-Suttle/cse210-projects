859d934d73fba9b0588c6d407e7ccb4406fe9837
Hunter Suttle <huntermsuttle.tfa@gmail.com>
2023-07-18 20:40:01 -0600

    Foundation1 Updated

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,16 +1,14 @@
-//Foundation 1
-
 using System;
 using System.Collections.Generic;
 
-public class Video
+public abstract class Media
 {
     public string Title { get; set; }
     public string Author { get; set; }
     public int Length { get; set; }
     public List<Comment> Comments { get; set; }
 
-    public Video(string title, string author, int length)
+    public Media(string title, string author, int length)
     {
         Title = title;
         Author = author;
@@ -18,7 +16,16 @@ public class Video
         Comments = new List<Comment>();
     }
 
-    public int GetNumberOfComments()
+    public abstract int GetNumberOfComments();
+}
+
+public class Video : Media
+{
+    public Video(string title, string author, int length) : base(title, author, length)
+    {
+    }
+
+    public override int GetNumberOfComments()
     {
         return Comments.Count;
     }
@@ -40,41 +47,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<Video> videos = new List<Video>();
+        List<Media> mediaList = new List<Media>();
 
-        Video video1 = new Video("Funny Cats", "Sam", 120);
+        Media video1 = new Video("Funny Cats", "Sam", 120);
         video1.Comments.Add(new Comment("Tim", "Super funny! lol"));
         video1.Comments.Add(new Comment("Tom", "I hate cats..."));
         video1.Comments.Add(new Comment("Tony", "10/10 would recommend"));
-        videos.Add(video1);
+        mediaList.Add(video1);
 
-        Video video2 = new Video("Laughing Baby Compilation", "Brooke", 180);
+        Media video2 = new Video("Laughing Baby Compilation", "Brooke", 180);
         video2.Comments.Add(new Comment("Bill", "Aww adorable!"));
         video2.Comments.Add(new Comment("Bob", "Reminds me of my babies when I was young"));
         video2.Comments.Add(new Comment("Brad", "Could not stop laughing!"));
-        videos.Add(video2);
+        mediaList.Add(video2);
 
-        Video video3 = new Video("How to Change a Tire", "Jane", 240);
+        Media video3 = new Video("How to Change a Tire", "Jane", 240);
         video3.Comments.Add(new Comment("Jim", "Saved my life! I was stuck on the freeway"));
         video3.Comments.Add(new Comment("Joe", "Very helpful for people"));
         video3.Comments.Add(new Comment("Jack", "Audio was terrible. Could barely hear anything with the wind."));
-        videos.Add(video3);
+        mediaList.Add(video3);
 
-        foreach (Video video in videos)
+        foreach (Media media in mediaList)
         {
-            Console.WriteLine("Title: " + video.Title);
-            Console.WriteLine("Author: " + video.Author);
-            Console.WriteLine("Length: " + video.Length + " seconds");
-            Console.WriteLine("Number of comments: " + video.GetNumberOfComments());
+            Console.WriteLine("Title: " + media.Title);
+            Console.WriteLine("Author: " + media.Author);
+            Console.WriteLine("Number of comments: " + media.GetNumberOfComments());
+            Console.WriteLine("Length: " + media.Length + " seconds");
+            Console.WriteLine();
 
-            foreach (Comment comment in video.Comments)
+            Console.WriteLine("Comments:");
+            foreach (Comment comment in media.Comments)
             {
                 Console.WriteLine(comment.Name + ": " + comment.Text);
             }
 
             Console.WriteLine();
         }
-
-        Console.ReadLine();
     }
 }
